6e86fa97209605d52fdd7e5476ace01e0c511bb0
alcl2000 <amy.555.lewis@gmail.com>
2023-01-04 18:17:00 +0000

    importing env to run.py

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash
 import json
+if os.path.exists('env.py'):
+    import env
 
 app = Flask(__name__)
 
@@ -30,8 +32,10 @@ def about_member(member_name):
     return render_template('member.html', member=member)              
 
 
-@app.route('/contact')
+@app.route('/contact', methods=['GET', 'POST'])
 def contact():
+    if request.method == 'POST':
+        print(request.form.get('name'))
     return render_template('contact.html', page_title='Contact Us')
 
 
