eab7ceb8000953195a6fecd440f8b1cf10d168ad
alcl2000 <amy.555.lewis@gmail.com>
2023-01-05 10:49:15 +0000

    add: flashed messages to form page

--- run.py
+++ run.py
@@ -5,6 +5,7 @@ if os.path.exists('env.py'):
     import env
 
 app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY")
 
 
 @app.route('/')
@@ -35,7 +36,8 @@ def about_member(member_name):
 @app.route('/contact', methods=['GET', 'POST'])
 def contact():
     if request.method == 'POST':
-        print(request.form.get('name'))
+        flash('Thanks {}, we have recieved your message!'.format(
+            request.form.get('name')))
     return render_template('contact.html', page_title='Contact Us')
 
 
